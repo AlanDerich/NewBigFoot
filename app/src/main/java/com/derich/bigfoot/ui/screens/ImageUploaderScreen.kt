3a38ad6774bb359211f8612158fa45d434d237cd
Alan Derich <alangitonga15@gmail.com>
2024-01-08 09:46:44 +0300

    Firebase UI login error

--- app/src/main/java/com/derich/bigfoot/ui/screens/ImageUploaderScreen.kt
+++ app/src/main/java/com/derich/bigfoot/ui/screens/ImageUploaderScreen.kt
@@ -1,10 +1,14 @@
 package com.derich.bigfoot.ui.screens
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
-import android.os.Environment
+import android.os.Build
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -12,8 +16,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.derich.bigfoot.model.MemberDetails
 import com.derich.bigfoot.ui.screens.transactions.TransactionsViewModel
@@ -21,9 +25,7 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.runBlocking
 import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
+import java.io.FileOutputStream
 
 private const val FILE_PROVIDER_AUTHORITY = "com.derich.bigfoot.fileprovider"
 private lateinit var transactionsVM: TransactionsViewModel
@@ -36,31 +38,19 @@ fun ImageUploaderScreen(memberDetails: MemberDetails, transactionsViewModel: Tra
     val storage = FirebaseStorage.getInstance()
     val storageRef = storage.reference
     val context = LocalContext.current
-    val getContent =
-        rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-            // Handle the selected image URI
-            if (uri != null) {
-                // Call your image upload logic here
-                imageUploader(uri,context)
-            } else {
-                // Handle the case where the user did not select an image
-                Toast.makeText(context, "Error Selecting the Image", Toast.LENGTH_SHORT).show()
-            }
+    val getContent = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        // Handle the selected image URI
+        if (uri != null) {
+            // Call your image size reduction and upload logic here
+            imageUploader(reduceAndUploadImage(uri, context), context)
+        } else {
+            // Handle the case where the user did not select an image
+            Toast.makeText(context, "Error Selecting the Image", Toast.LENGTH_SHORT).show()
         }
+    }
 
-    val takePicture =
-        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { success: Boolean ->
-            if (success) {
-                // Handle the captured image
-                // Call your image upload logic here
-//                imageUploader(uri, context)
-            } else {
-                // Handle the case where image capture failed
-                Toast.makeText(context, "Image capture failed!", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-    Column {
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
         // UI elements here
         Button(
             onClick = {
@@ -71,39 +61,44 @@ fun ImageUploaderScreen(memberDetails: MemberDetails, transactionsViewModel: Tra
             Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
             Text("Select Image")
         }
-
-        Button(
-            onClick = {
-                // Launch the camera to capture an image
-                takePicture.launch(createImageCaptureUri(context))
-            }
-        ) {
-            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
-            Text("Take Picture")
-        }
     }
 }
-
-@Throws(IOException::class)
-private fun createImageCaptureUri(context: Context): Uri {
-    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-    val imageFileName = "JPEG_$timeStamp"
-    val storageDir: File? = ContextCompat.getExternalFilesDirs(context, Environment.DIRECTORY_PICTURES).firstOrNull()
-
-    return if (storageDir != null) {
-        val imageFile = File.createTempFile(
-            imageFileName,  /* prefix */
-            ".jpg",  /* suffix */
-            storageDir      /* directory */
-        )
-        FileProvider.getUriForFile(
-            context,
-            FILE_PROVIDER_AUTHORITY,
-            imageFile
-        )
+fun reduceAndUploadImage(originalUri: Uri, context: Context): Uri {
+    // Load the selected image into a Bitmap using an alternative method for lower API levels
+    val originalBitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, originalUri))
     } else {
-        throw IOException("Could not create image file.")
+        MediaStore.Images.Media.getBitmap(context.contentResolver, originalUri)
     }
+
+    // Specify the desired maximum width and height for the reduced image
+    val maxWidth = 800
+    val maxHeight = 800
+
+    // Calculate the new dimensions while maintaining the aspect ratio
+    val scale = calculateScaleFactor(originalBitmap.width, originalBitmap.height, maxWidth, maxHeight)
+    val newWidth = (originalBitmap.width * scale).toInt()
+    val newHeight = (originalBitmap.height * scale).toInt()
+
+    // Create a new Bitmap with reduced dimensions
+    val resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true)
+
+    // Create a temporary file using File.createTempFile
+    val reducedFile = File.createTempFile("reduced_image", ".jpg", context.cacheDir)
+
+    // Save the resized Bitmap to the temporary file
+    val outputStream = FileOutputStream(reducedFile)
+    resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream)
+    outputStream.close()
+
+    // Return the URI of the reduced image file
+    return FileProvider.getUriForFile(context, context.packageName + ".provider", reducedFile)
+}
+
+fun calculateScaleFactor(originalWidth: Int, originalHeight: Int, maxWidth: Int, maxHeight: Int): Float {
+    val widthScale = maxWidth.toFloat() / originalWidth.toFloat()
+    val heightScale = maxHeight.toFloat() / originalHeight.toFloat()
+    return if (widthScale < heightScale) widthScale else heightScale
 }
 
 fun imageUploader(
@@ -127,7 +122,7 @@ fun imageUploader(
             // Handle the download URL
             updateMemberDetails(context, downloadUrl)
         }
-    }.addOnFailureListener { exception ->
+    }.addOnFailureListener {
         // Handle unsuccessful uploads
         Toast.makeText(context, "Error occurred while uploading the Image", Toast.LENGTH_SHORT).show()
     }

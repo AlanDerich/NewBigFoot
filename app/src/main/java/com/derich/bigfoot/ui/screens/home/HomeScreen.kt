91864151e5ba775b4592999ff99c3fc5ef11a0b9
AlanDerich <alangitonga15@gmail.com>
2024-04-22 13:32:15 +0300

    updated home screen

--- app/src/main/java/com/derich/bigfoot/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/derich/bigfoot/ui/screens/home/HomeScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -93,7 +94,7 @@ fun ContributionCard(contribution: MemberDetails,
                 loading = placeholder(R.drawable.bigfut1),
                 failure = placeholder(R.drawable.bigfut1),
                 modifier = Modifier
-                    .clip(MaterialTheme.shapes.medium)
+                    .clip(CircleShape)
                     .size(68.dp)
             )
 //            Image(
